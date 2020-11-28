29b3b7fc70152ac013e80a68bb4e9bfd41ae2e98
Ankitkj1999 <57802410+Ankitkj1999@users.noreply.github.com>
2020-11-28 15:58:44 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,21 +13,33 @@ void main() {
                   height: 300,
                   child: Center(
                     child: Stack(
+                      fit: StackFit.expand,
+                      clipBehavior: Clip.antiAliasWithSaveLayer,
+                      overflow: Overflow.visible,
+
                       children: <Widget>[
                         Container(
                           width: 300,
                           height: 300,
                           color: Colors.red,
                         ), //Container
-                        Container(
-                          width: 250,
-                          height: 250,
-                          color: Colors.black,
+                        Positioned(
+                          top: 80,
+                          left: 80,
+                          child: Container(
+                            width: 250,
+                            height: 250,
+                            color: Colors.black,
+                          ),
                         ), //Container
-                        Container(
-                          height: 200,
-                          width: 200,
-                          color: Colors.purple,
+                        Positioned(
+                          left: 20,
+                          top: 20,
+                          child: Container(
+                            height: 200,
+                            width: 200,
+                            color: Colors.purple,
+                          ),
                         ), //Container
                       ], //<Widget>[]
                     ), //Stack
